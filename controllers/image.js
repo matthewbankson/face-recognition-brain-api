7b1f66b5e06878728d0d0ca1ac2fa8222377c184
matthewbankson <86269497+matthewbankson@users.noreply.github.com>
2021-08-19 16:17:02 -0500

    security updates

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,18 @@
+import Clarifai from 'clarifai';
+
+const app = new Clarifai.App({
+  apiKey: 'b51f429601e148c4994a1297749e31ea',
+});
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(400).json('unable to work with API'));
+};
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users')
@@ -10,4 +25,4 @@ const handleImage = (req, res, db) => {
     .catch((err) => res.status(400).json('unable to get entries'));
 };
 
-export default handleImage;
+export { handleImage, handleApiCall };
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt-nodejs';
 import handleRegister from './controllers/register.js';
 import handleSignin from './controllers/signin.js';
 import handleProfileGet from './controllers/profile.js';
-import handleImage from './controllers/image.js';
+import { handleApiCall, handleImage } from './controllers/image.js';
 
 const db = knex({
   client: 'pg',
@@ -39,6 +39,10 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   handleImage(req, res, db);
+});
+
+app.post('/imageurl', (req, res) => {
+  handleApiCall(req, res);
 });
 
 app.listen(3000, () => {
