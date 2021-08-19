2896368398419f24fe7aa1fd8d0690cf9b718a4d
matthewbankson <86269497+matthewbankson@users.noreply.github.com>
2021-08-19 17:31:32 -0500

    added database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { handleApiCall, handleImage } from './controllers/image.js';
 const db = knex({
   client: 'pg',
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
