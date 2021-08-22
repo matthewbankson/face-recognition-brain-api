687b7a980183a2c549c59489c91e228233470bf1
matthewbankson <86269497+matthewbankson@users.noreply.github.com>
2021-08-22 17:22:55 -0400

    database

--- server.js
+++ server.js
@@ -7,6 +7,14 @@ import handleSignin from './controllers/signin.js';
 import handleProfileGet from './controllers/profile.js';
 import { handleApiCall, handleImage } from './controllers/image.js';
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
 const db = knex({
   client: 'pg',
   connection: {
