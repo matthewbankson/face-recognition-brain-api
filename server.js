e40658a0e26115e7d12a5757a148f0b040f73783
matthewbankson <86269497+matthewbankson@users.noreply.github.com>
2021-08-22 21:34:03 -0400

    database

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import handleRegister from './controllers/register.js';
 import handleSignin from './controllers/signin.js';
 import handleProfileGet from './controllers/profile.js';
 import { handleApiCall, handleImage } from './controllers/image.js';
+import path from 'path/posix';
 
 const db = knex({
   client: 'pg',
