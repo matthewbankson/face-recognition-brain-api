87f8b34026c3c79061bc10c52cc1d3a9c7a56c2d
matthewbankson <86269497+matthewbankson@users.noreply.github.com>
2021-08-22 22:30:41 -0400

    database

--- server.js
+++ server.js
@@ -36,13 +36,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
-// app.get('*', (request, response) => {
-//   response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+app.get('*', (request, response) => {
+  response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 app.get('/', (req, res) => {
   res.send('success');
