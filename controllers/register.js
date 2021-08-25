143b66a25e849e54d6df27bf19907f9df94c4781
matthewbankson <86269497+matthewbankson@users.noreply.github.com>
2021-08-25 12:36:39 -0500

    database

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,7 @@
 const handleRegister = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
   if (!email || !name || !password) {
+    console.log('ehllo');
     return res.status(400).json('incorrect form submission');
   }
   const hash = bcrypt.hashSync(password);
